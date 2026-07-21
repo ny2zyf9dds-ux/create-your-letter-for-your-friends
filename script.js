a3efc5f6fe1de4c4f6ecca7f35bf62c53bc00720
ny2zyf9dds-ux <ny2zyf9dds@privaterelay.appleid.com>
2026-07-21 13:03:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Dearly is starting...");
+const envelope = document.getElementById("envelope");
+const createButton = document.getElementById("createButton");
+
+envelope.addEventListener("click", () => {
+    envelope.classList.toggle("open");
+});
+
+createButton.addEventListener("click", () => {
+    window.location.href = "create.html";
+});
